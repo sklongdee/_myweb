1ef01633c43054898e35546a83c3dc0db89d3793
suttipong klongdee <stp.suttipong@gmail.com>
2025-05-03 13:06:30 +0700

    Initial commit

---
+++ admin/activity.php
@@ -0,0 +1,25 @@
+<h1>หน้าข่าวสาร</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">รูปภาพ</th>
+      <th scope="col">หัวข้อข่าว</th>
+      <th scope="col">วันที่ประกาศ</th>
+      <th scope="col">จัดการ</th>
+    </tr>
+  </thead>
+  <tbody>
+
+
+    <tr>
+      <th scope="row">1</th>
+      <td><img src="../img/83851picture.jpg" width="80" height="auto" alt=""></td>
+      <td>มทร.ตะวันออก ร่วมพิธีมอบรางวัลบุคลากรดีเด่น และหน่วยงานดีเด่น ของกรมประชาสัมพันธ์ เนื่องในงานวันสถาปนากรมประชาสัมพันธ์ ปีที่ 92</td>
+      <td>2025-05-03</td>
+      <td>แก้ไข ลบ</td>
+    </tr>
+
+    
+  </tbody>
+</table>
---
+++ main.php
@@ -0,0 +1,62 @@
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/1744789623310.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/eit_ita2025.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/NoGiftPolicy.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+    <h1>กิจกรรม</h1>
+    <div class="row row-cols-1 row-cols-md-5 g-4 md-4">
+
+    <?php
+    include_once "conn.php";
+    $sql = "SELECT * FROM activity";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+    ?> 
+
+        <div class="col">
+            <div class="card h-100">
+            <img src="img/<?=$row["activity_img"]?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text"><?=$row["activity_title"]?></p>
+            </div>
+            <div class="card-footer">
+                <small class="text-body-secondary"><?=$row["activity_date"]?></small>
+            </div>
+            </div>
+        </div>
+
+    <?php
+    }
+    } else {
+    echo "0 results";
+    }
+    $conn->close();
+    ?>
+
+    </div>
+</div>
+<p></p>
